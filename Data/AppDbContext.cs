2bb0fd168a8256c8ed55df485be599a5c330fa0b
KhanyisoV <khanyisovabaza@gmail.com>
2026-01-14 12:09:33 +0200

    Initial Commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using JobApplicationTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobApplicationTracker.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+        public DbSet<JobApplication> JobApplications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<JobApplication>()
+                .Property(j => j.Status)
+                .HasConversion<string>();
+
+        }
+
+    }
+}
---
+++ Models/JobApplication.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace JobApplicationTracker.Models
+
+{
+    public enum ApplicationStatus
+    {
+        Accepted, Rejected, Awaiting
+
+    }
+    public class JobApplication
+    {
+
+        [Key]
+        public int JobId { get; set; }
+        public string Name { get; set; }
+        public string? JobDescription { get; set; }
+        public ApplicationStatus Status { get; set; }
+
+        public DateTime? Modified { get; set; }
+
+        public DateTime? AppliedDate { get; set; }
+       
+
+    }
+}

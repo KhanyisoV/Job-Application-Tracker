2782d05e4ba9360fd61d121c05c8bf74a7204aa3
KhanyisoV <khanyisovabaza@gmail.com>
2026-01-18 20:53:02 +0200

    backend url bug fix

--- job-app-tracker-frontend/src/App.js
+++ job-app-tracker-frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
 
-    const apiUrl = "https://jobtracker-backend-f7bxc9fyg4htendh.southafricanorth-01.azurewebsites.net/api";
+    const apiUrl = "https://jobtracker-backend-f7bxc9fyg4htendh.southafricanorth-01.azurewebsites.net";
   const [applications, setApplications] = useState([]);
   const [name,setName] = useState("");
   const [status,setStatus] = useState("Awaiting");
